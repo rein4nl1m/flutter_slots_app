5ff97e23829997058c23ade1671326c3027fc863
Reinan Pereira Lima <reinan.lima@hbsis.com.br>
2020-08-23 23:05:39 -0300

    :art: Improved status and information messages;

--- lib/consts.dart
+++ lib/consts.dart
@@ -1,11 +1,5 @@
 const String kApple = "assets/images/apple.png";
-const String kApple2x = "assets/images/apple@2x.png";
-const String kApple3x = "assets/images/apple@3x.png";
 const String kCherry = "assets/images/icons8-cherry.png";
-const String kCherry2x = "assets/images/icons8-cherry@2x.png";
-const String kCherry3x = "assets/images/icons8-cherry@3x.png";
 const String kStar = "assets/images/icons8-star-512.png";
-const String kStar2x = "assets/images/icons8-star-512@2x.png";
-const String kStar3x = "assets/images/icons8-star-512@3x.png";
 
 const kSlot = [kApple, kCherry, kStar];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_slots_app/pages/widgets/slot_container.dart';
 import 'package:flutter_slots_app/pages/widgets/title_row.dart';
@@ -11,8 +10,15 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _score = 100;
-  final _randomNumbers = [0, 0, 0];
+  List<int> _randomNumbers = [0, 0, 0];
   final Random random = Random();
+
+  void reset() {
+    setState(() {
+      _score = 100;
+      _randomNumbers = [0, 0, 0];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,6 +37,24 @@ class _HomePageState extends State<HomePage> {
           Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
+              Expanded(
+                flex: 1,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.info),
+                      color: Colors.white,
+                      onPressed: _showInfo,
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.refresh),
+                      color: Colors.white,
+                      onPressed: reset,
+                    ),
+                  ],
+                ),
+              ),
               TittleRow(),
               Container(
                 padding: EdgeInsets.all(3.0),
@@ -41,13 +65,16 @@ class _HomePageState extends State<HomePage> {
                 width: 110,
                 child: Text('Credits: $_score'),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  SlotContainer(randomNumber: _randomNumbers[0]),
-                  SlotContainer(randomNumber: _randomNumbers[1]),
-                  SlotContainer(randomNumber: _randomNumbers[2])
-                ],
+              Expanded(
+                flex: 3,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SlotContainer(randomNumber: _randomNumbers[0]),
+                    SlotContainer(randomNumber: _randomNumbers[1]),
+                    SlotContainer(randomNumber: _randomNumbers[2])
+                  ],
+                ),
               ),
               GestureDetector(
                 child: Container(
@@ -68,28 +95,174 @@ class _HomePageState extends State<HomePage> {
                 ),
                 onTap: () {
                   if (_score > 0) {
-                    setState(() {
-                      _randomNumbers[0] = random.nextInt(3);
-                      _randomNumbers[1] = random.nextInt(3);
-                      _randomNumbers[2] = random.nextInt(3);
+                    setState(
+                      () {
+                        _randomNumbers[0] = random.nextInt(3);
+                        _randomNumbers[1] = random.nextInt(3);
+                        _randomNumbers[2] = random.nextInt(3);
 
-                      if (_randomNumbers[0] == _randomNumbers[1] &&
-                          _randomNumbers[1] == _randomNumbers[2] &&
-                          _randomNumbers[2] == _randomNumbers[0]) {
-                        _score += 10;
-                      } else {
-                        if(_score >= 5){
+                        if (_randomNumbers[0] == _randomNumbers[1] &&
+                            _randomNumbers[1] == _randomNumbers[2] &&
+                            _randomNumbers[2] == _randomNumbers[0]) {
+                          _score += 10;
+                          _showWinner();
+                        } else {
                           _score -= 5;
                         }
-                      }
-                    });
+                      },
+                    );
+                  } else {
+                    _noCredits();
                   }
                 },
               ),
+              Spacer()
             ],
           )
         ],
       ),
+    );
+  }
+
+  void _showWinner() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          contentPadding: EdgeInsets.only(top: 10),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          title: const Text(
+            "You Win!",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          content: const Text(
+            "+10 Credits",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: const Text(
+                "Ok",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _noCredits() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          contentPadding: EdgeInsets.only(top: 10),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          title: const Text(
+            "Sorry...",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          content: const Text(
+            "You have no more credits.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: const Text(
+                "Ok",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showInfo() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          contentPadding: EdgeInsets.only(top: 10, left: 8, right: 8),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          title: const Text(
+            "Info:",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          content: RichText(
+            text: TextSpan(
+              text: "Every win, you get ",
+              style: TextStyle(color: Colors.black),
+              children: <TextSpan>[
+                TextSpan(
+                  text: "10 credits!",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                TextSpan(
+                  text: "\n\nEvery defeat, you lose ",
+                ),
+                TextSpan(
+                  text: "5 credits ...",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: const Text(
+                "Ok",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
